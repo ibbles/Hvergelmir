8b21267673ec0eedc184a62a44f59d437897ef9e
Martin Nilsson <ibbles@hotmail.com>
2014-08-23 18:57:37 +0200

    Added Stack.py.
    With file header. Contents to follow.

---
+++ source/errors/Stack.py
@@ -0,0 +1,7 @@
+"""
+Classes representing a call stack.
+
+Part of Hvergelmir, a tree based Valgrind output viewer - https://github.com/ibbles/Hvergelmir
+See LICENSE for licensing information.
+"""
+
