555387f440e9408a408c4b1e36ceddd7f71ca081
Martin Nilsson <ibbles@hotmail.com>
2014-09-05 18:40:32 +0200

    SourceCodePanel can now be given some source code to display.
    Also accepts a line number to highlight.

--- source/gui/SourceCodePanel.py
+++ source/gui/SourceCodePanel.py
@@ -9,6 +9,11 @@ import wx.stc
 
 
 
+def appendNewline(string):
+  return string+"\n"
+
+
+
 class SourceCodePanel(wx.Panel):
   def __init__(self, parent, initialText):
     """ A source code panel displays source code in a non-editable text area.
@@ -27,3 +32,36 @@ class SourceCodePanel(wx.Panel):
     sizer = wx.BoxSizer(wx.VERTICAL)
     sizer.Add(self.sourceCode, 1, wx.EXPAND)
     self.SetSizer(sizer)
+
+
+
+
+
+
+
+  def setSouceCode(self, lines, highlightedLine): # None
+    """Display 'lines'. If 'highlightedLine' is given then that line will be
+    highlighted and brought into view.
+    @param lines - string list - Content to be displayed.
+    @param highlightedLine - integer - Line number to highlight"""
+
+  
+
+    self.sourceCode.SetReadOnly(False)
+    self.sourceCode.ClearAll()
+    self.sourceCode.AddText(''.join(map(appendNewline, lines)))
+    if highlightedLine != None and len(lines) > highlightedLine:
+      self.highlightLine(lines, highlightedLine)
+    self.sourceCode.SetReadOnly(True)
+
+
+
+  def highlightLine(self, lines, highlightedLine): # None
+    firstChar = 0
+    for i in range(0, highlightedLine-1):
+      firstChar += len(lines[i])+1
+    lastChar = firstChar + len(lines[highlightedLine-1])
+
+    self.sourceCode.ScrollToLine(highlightedLine-1 - 10)
+    self.sourceCode.SetSelection(firstChar, lastChar)
+
