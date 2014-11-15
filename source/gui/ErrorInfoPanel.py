44e507ced575090108a8689c179df0f5c2732c16
Martin Nilsson <ibbles@hotmail.com>
2014-11-15 13:51:17 +0100

    Code formating dictated by ninja-ide.

--- source/gui/ErrorInfoPanel.py
+++ source/gui/ErrorInfoPanel.py
@@ -5,22 +5,25 @@ See LICENSE for licensing information.
 
 import wx
 
+
 class ErrorInfoPanel(wx.Panel):
-  def __init__(self, parent):
-    wx.Panel.__init__(self, parent=parent)
+    """"""
 
-    self.text = wx.TextCtrl(self, style=wx.TE_MULTILINE|wx.TE_READONLY)
+    def __init__(self, parent):
+        """"""
+        wx.Panel.__init__(self, parent=parent)
 
-    sizer = wx.BoxSizer(wx.VERTICAL)
-    sizer.Add(self.text, 1, wx.EXPAND)
-    self.SetSizer(sizer)
+        self.text = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY)
 
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(self.text, 1, wx.EXPAND)
+        self.SetSizer(sizer)
 
-  def display(self, error):
-    self.clear()
-    self.text.write(error.info())
+    def display(self, error):
+        """"""
+        self.clear()
+        self.text.write(error.info())
 
-
-
-  def clear(self):
-    self.text.Clear()
+    def clear(self):
+        """"""
+        self.text.Clear()
